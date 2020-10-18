d771314320a11715f7d6949a0f798f02b5a3433b
Ettore Galli <ettore.galli@gmail.com>
2020-10-18 21:58:40 +0200

    status

--- led/switchserver/views/switch_pattern_api.py
+++ led/switchserver/views/switch_pattern_api.py
@@ -8,13 +8,17 @@ class SwitchPatternApi(MethodView):
         
     def post(self):
         input_pattern = request.json["pattern"]
-        pattern = self.__prepare_led_pattern(request.json["pattern"])
-        print(input_pattern)
+        current_app.status_manager.set_whole_status(self.__prepare_status(input_pattern))
+        pattern = self.__prepare_led_pattern(
+            current_app.status_manager.get_whole_status()
+        )
         current_app.switch_driver.set_output_pattern(*pattern) 
         return request.json
 
-    def __prepare_led_pattern(self, json_pattern):
-        return [v for s, v in sorted(json_pattern.items())]
+    def __prepare_status(self, json_pattern):
+        return [(s, v) for s, v in sorted(json_pattern.items())]
 
+    def __prepare_led_pattern(self, status):
+       return [v for s, v in status]
         
     
