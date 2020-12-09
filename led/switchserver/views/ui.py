94e8dbb71a9841edf9a4e4c017c67399c0101b3b
Ettore Galli <ettore.galli@gmail.com>
2020-12-09 22:34:36 +0100

    get status via api

--- led/switchserver/views/ui.py
+++ led/switchserver/views/ui.py
@@ -1,15 +1,14 @@
-from flask import request, session, current_app, render_template
+from flask import render_template
 from flask.views import MethodView
 
+
 class Ui(MethodView):
- 
+
     def get(self):
         return self.__prepare_index_page()
-
 
     def post(self):
         return self.__prepare_index_page()
 
     def __prepare_index_page(self):
-        response = render_template("index.html")
-        return response
+        return render_template("index.html")
