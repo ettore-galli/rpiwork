5b8f69e1df74fb0c16f530424b4737320cd7f5e4
Ettore Galli <ettore.galli@gmail.com>
2020-12-14 19:14:17 +0100

    host

--- led/mac_env.sh
+++ led/mac_env.sh
@@ -1,4 +1,4 @@
 export VIRTUALENV_ACTIVATE=./venv/bin/activate
 export FLASK_ENV=development
 export PYTHONPATH=$PYTHONPATH:$(pwd)/..
-export REACT_APP_SWITCH_SERVER_ENDPOINT=http://localhost:8080/pattern/
+export REACT_APP_SWITCH_SERVER_ENDPOINT=http://$(hostname):8080/pattern/
--- led/switchclient/src/config/endpoint.js
+++ led/switchclient/src/config/endpoint.js
@@ -1,3 +1,5 @@
+const REACT_APP_SWITCH_SERVER_ENDPOINT_PORT=8080;
+
 export const getSwitchPatternURL = () => {
-    return process.env.REACT_APP_SWITCH_SERVER_ENDPOINT;
+    return window.location.protocol + "//" + window.location.hostname + ":" + REACT_APP_SWITCH_SERVER_ENDPOINT_PORT + "/pattern/";
 }
