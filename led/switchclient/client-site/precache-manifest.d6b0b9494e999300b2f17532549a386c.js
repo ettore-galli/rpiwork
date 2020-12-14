89d256b1880f1706f52ee91a902cb20150d8cf40
Ettore Galli <ettore.galli@gmail.com>
2020-12-14 19:28:38 +0100

    m

---
+++ led/switchclient/client-site/precache-manifest.d6b0b9494e999300b2f17532549a386c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6558a8b3eb929c01c6e9c77380aee69e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea5bed3a9812320232de",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a8bf3d749e4e1e02070a",
+    "url": "/static/js/2.950358b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.950358b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea5bed3a9812320232de",
+    "url": "/static/js/main.b18a25c1.chunk.js"
+  },
+  {
+    "revision": "fdda4e133daae4b574db",
+    "url": "/static/js/runtime-main.3896fae3.js"
+  }
+]);
