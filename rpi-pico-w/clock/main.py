8aa64280be8caaa2dbf0f0e4ced4d174f188c3b6
Ettore Galli <ettore.galli@gmail.com>
2022-08-27 10:28:04 +0200

    Temp sensor

--- rpi-pico-w/clock/main.py
+++ rpi-pico-w/clock/main.py
@@ -1,7 +1,7 @@
 import time
 import uasyncio as asyncio
 
-from machine import Pin, RTC
+from machine import Pin, RTC, ADC
 
 from sysfont import sysfont
 from pico_lcd_18 import LCD_1inch8, SPI0_WIRING
@@ -49,28 +49,30 @@ def render_big_time(lcd, dsp_time, color):
     pos_h = (height - 8 * aSize) // 2
     lcd.draw_text((5, pos_h), dsp_time, color, sysfont, aSize=aSize)
 
+
 def render_temperature(lcd, temperature, pos_w, pos_h, color):
     temperature_text = str(temperature[0]) + " " + str(temperature[1])
 
     lcd.text(temperature_text, pos_w, pos_h, color)
 
-def render_display(lcd, message="", curtime=(0, 0, 0, 0, 0, 0), temperature=(0, "C")):
-    
+
+def render_display(
+    lcd, message="", curtime=(0, 0, 0, 0, 0, 0), temperature=(-273.15, "C")
+):
+
     dsp_time = fmt_time_hm(curtime)
     seconds = curtime[-1]
 
     lcd.fill(LCD.BLACK)
 
-
-    
-    render_big_time_color =  lcd.rgb565(0, 255, 0)
+    render_big_time_color = lcd.rgb565(0, 255, 0)
     render_big_time(lcd=lcd, dsp_time=dsp_time, color=render_big_time_color)
-    
+
     render_multiline_text(lcd=lcd, text=message, pos_w=2, pos_h=8, color=LCD.WHITE)
-    
+
     render_seconds_bar(lcd, 10, 100, 130, 10, LCD.WHITE, seconds)
-    
-    render_temperature_color=0x4BC5
+
+    render_temperature_color = 0x4BC5
     render_temperature(lcd, temperature, 10, 115, render_temperature_color)
 
     lcd.show()
@@ -81,6 +83,7 @@ def draw_display(lcd, display_data):
         lcd=lcd,
         message=display_data.get("message", ""),
         curtime=display_data.get("time_hms", (0, 0, 0, 0, 0, 0)),
+        temperature=(display_data.get("temperature", -273.15), "C"),
     )
 
 
@@ -130,6 +133,19 @@ async def retrieve_time(display_data_time_updater, retrieve_delay_ms=10):
         await asyncio.sleep_ms(retrieve_delay_ms)
 
 
+async def retrieve_temp(
+    temperature_sensor_reader, display_data_temp_updater, retrieve_delay_ms=1000
+):
+    conversion_factor = 3.3 / (65535)
+    while True:
+        reading = temperature_sensor_reader() * conversion_factor
+        temperature = 27 - (reading - 0.706) / 0.001721
+
+        display_data_temp_updater(temperature)
+
+        await asyncio.sleep_ms(retrieve_delay_ms)
+
+
 async def connect_wlan(run_environment):
     try:
         _ = get_wlan_connection(run_environment=run_environment)
@@ -148,8 +164,14 @@ async def main(run_environment):
     def display_data_time_updater(time_hms):
         display_data["time_hms"] = time_hms
 
+    def display_data_temp_updater(temperature):
+        display_data["temperature"] = temperature
+
     def display_data_retriever():
         return display_data.copy()
+
+    def temperature_sensor_reader():
+        return temp_sensor.read_u16()
 
     async def sync_ntp_time_worker(display_message_updater):
         await sync_ntp_time(
@@ -181,6 +203,12 @@ async def main(run_environment):
         asyncio.create_task(
             sync_ntp_time_worker(display_message_updater=display_message_updater)
         ),
+        asyncio.create_task(
+            retrieve_temp(
+                temperature_sensor_reader=temperature_sensor_reader,
+                display_data_temp_updater=display_data_temp_updater,
+            )
+        ),
     ]
 
     for task in tasks:
@@ -193,6 +221,8 @@ if __name__ == "__main__":
     run_environment = RunEnvironment(
         network_parameters=network_parameters, logger=logger
     )
+
+    temp_sensor = ADC(4)
 
     wiring = SPI0_WIRING
 
