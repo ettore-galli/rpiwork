70542770fda2a914d50b8cad51e2e59eb23ab40a
Ettore Galli <ettore.galli@gmail.com>
2022-08-28 14:37:53 +0200

    improve font dsp

--- rpi-pico-w/clock/fontdsp.py
+++ rpi-pico-w/clock/fontdsp.py
@@ -6,6 +6,7 @@ def pick_char_data(font_map, input_char):
 
     fontw = font_map["Width"]
     fonth = font_map["Height"]
+    
     ch = ord(input_char) if isinstance(input_char, str) else input_char
 
     ci = (ch - startchar) * fontw
@@ -25,14 +26,17 @@ def dsp_char(fontw, fonth, char_data):
         for r in range(fonth):
             if c >> r & 0x01:
                 rendered[r][i] = "*"
-
+            else:    
+                rendered[r][i] = " "
     return rendered
 
 
 if __name__ == "__main__":
-    for ichar in range(254):
+    for ichar in range(32, 212):
         fontw, fonth, char_data = pick_char_data(sysfont, ichar)
+        print (f"\n----- [{chr(ichar)}] -----\n")
         print([hex(int(chard)) for chard in char_data])
         print([(int(chard)) for chard in char_data])
+        print("\n")
         for line in dsp_char(fontw, fonth, char_data):
             print(" ".join(line))
