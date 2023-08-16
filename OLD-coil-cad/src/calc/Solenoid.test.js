d7237785ddff5b90561ed0f192fadc82b1736d49
ettore-galli <ettore.galli@gmail.com>
2023-08-16 14:06:40 +0200

    Cleanup

--- OLD-coil-cad/src/calc/Solenoid.test.js
+++
@@ -1,6 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import inductance from './Solenoid';
-
-test('inductance gives a value', () => {
-  expect(inductance(1, 1, 1, 1)).toBeCloseTo(9.86931332662495e-7, 3);
-});
