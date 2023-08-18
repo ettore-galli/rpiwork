0bb0de9a36fade09ea64929dfab15ab63236eec6
ettore-galli <ettore.galli@gmail.com>
2023-08-18 14:56:47 +0200

    Working display example

---
+++ rpi-pico-w/ssd1306demo/README.md
@@ -0,0 +1,6 @@
+# SSD1306
+
+## Docs
+
+<https://peppe8o.com/ssd1306-i2c-oled-raspberry-pi-pico-micropython/>
+
---
+++ rpi-pico-w/ssd1306demo/pico-oled-peppe8o.py
@@ -0,0 +1,39 @@
+#              .';:cc;.
+#            .,',;lol::c.
+#            ;';lddddlclo
+#            lcloxxoddodxdool:,.
+#            cxdddxdodxdkOkkkkkkkd:.
+#          .ldxkkOOOOkkOO000Okkxkkkkx:.
+#        .lddxkkOkOOO0OOO0000Okxxxxkkkk:
+#       'ooddkkkxxkO0000KK00Okxdoodxkkkko
+#      .ooodxkkxxxOO000kkkO0KOxolooxkkxxkl
+#      lolodxkkxxkOx,.      .lkdolodkkxxxO.
+#      doloodxkkkOk           ....   .,cxO;
+#      ddoodddxkkkk:         ,oxxxkOdc'..o'
+#      :kdddxxxxd,  ,lolccldxxxkkOOOkkkko,
+#       lOkxkkk;  :xkkkkkkkkOOO000OOkkOOk.
+#        ;00Ok' 'O000OO0000000000OOOO0Od.
+#         .l0l.;OOO000000OOOOOO000000x,
+#            .'OKKKK00000000000000kc.
+#               .:ox0KKKKKKK0kdc,.
+#                      ...
+#
+# Author: peppe8o
+# Date: Jun 12th, 2022
+# Version: 1.0
+
+from machine import Pin, I2C
+from ssd1306 import ssd1306
+
+# setup the I2C communication
+i2c = I2C(0, sda=Pin(16), scl=Pin(17))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+
+
+# The following part changes according to what you want to display
+display.text('Hello,', 0, 0)
+display.text('peppe8o.com', 0, 16)
+display.text('readers!', 0, 32)
+
+# The following line sends what to show to the display
+display.show()
