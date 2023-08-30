c84db0519323cd6d770d3a92347bfe768c31f04a
ettore-galli <ettore.galli@gmail.com>
2023-08-30 22:54:16 +0200

    duration

--- rpi-pico-w/tinygo/blinky/main.go
+++ rpi-pico-w/tinygo/blinky/main.go
@@ -6,13 +6,14 @@ import (
 )
 
 func main() {
+	var delayMs float64 = 100
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	for {
 		led.Low()
-		time.Sleep(time.Millisecond * 500)
+		time.Sleep(time.Millisecond * time.Duration(delayMs))
 
 		led.High()
-		time.Sleep(time.Millisecond * 500)
+		time.Sleep(time.Millisecond * time.Duration(delayMs))
 	}
 }
